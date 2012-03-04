8ceb28eff29c7d02a39732875037f89304298e07
Rocky Wilkins <rocky.wilkins@internetware.co.uk>
2012-03-04 15:55:59 +0000

    Add properties to camera and a small refactor for good coding practices

--- Engine/Graphics/Camera.cs
+++ Engine/Graphics/Camera.cs
@@ -7,65 +7,103 @@ namespace Engine.Graphics
 {
 	public class Camera
 	{
-		private List<Entity> visableEntities = new List<Entity>();
+		#region Fields
 
-		private Vector2 position;
+		private Vector2 position = new Vector2();
+		private float zoom = 1f;
+		private float rotation = 0f;
+
 		private Matrix transform;
-		private float zoom;
-		private float rotation;
 
-		private float screenWidth;
-		private float screenHeight;
+		private int width;
+		private int height;
+
+		#endregion
+
+
+		#region Properties
+
+		public Vector2 Position
+		{
+			get { return position; }
+			set { position = value; }
+		}
+
+		public float Zoom
+		{
+			get { return zoom; }
+			set { zoom = value; }
+		}
+
+		public float Rotation
+		{
+			get { return rotation; }
+			set { rotation = value; }
+		}
 
 		public Matrix Transform
 		{
-			get 
-			{
-				transform = Matrix.CreateTranslation(new Vector3(-position.X, -position.Y, 0)) *
-										Matrix.CreateRotationZ(rotation) *
-										Matrix.CreateScale(new Vector3(zoom, zoom, 1)) *
-										Matrix.CreateTranslation(new Vector3(screenWidth * 0.5f, screenHeight * 0.5f, 0));
-
-				return transform; 
-			}
+			get { return transform; }
 			set { transform = value; }
 		}
 
-		public Camera()
+		public int Width
 		{
-			position = new Vector2(0f, 0f);
-			zoom = 1f;
-			rotation = 0f;
-
-			screenWidth = 800f;
-			screenHeight = 480f;
+			get { return width; }
+			set { width = value; }
 		}
 
-		public void GetVisableEntites(IEnumerable<Entity> entities)
+		public int Height
 		{
-			visableEntities.Clear();
+			get { return height; }
+			set { height = value; }
+		}
+
+		#endregion
+
+
+		#region Methods
+
+		public Camera(int width, int height)
+		{
+			// Set the camera dimensions
+			this.width = width;
+			this.height = height;
+		}
+
+		public List<Entity> GetVisibleEntites(IEnumerable<Entity> entities)
+		{
+			List<Entity> visibleEntities = new List<Entity>();
 
 			foreach (Entity entity in entities)
 			{
-				// Add code for checking if entity is in screen bounds
-				visableEntities.Add(entity);
+				// TODO: Add code for checking if entity is in screen bounds
+
+				visibleEntities.Add(entity);
 			}
+
+			return visibleEntities;
+		}
+
+		public void Update(GameTime gameTime)
+		{
+			// Create the matrix for transforming sprites to camera space
+			transform = Matrix.CreateTranslation(new Vector3(-position.X, -position.Y, 0)) *
+										Matrix.CreateRotationZ(rotation) *
+										Matrix.CreateScale(new Vector3(zoom, zoom, 1)) *
+										Matrix.CreateTranslation(new Vector3(width / 2f, height / 2f, 0));
 		}
 
 		public void Draw(IEnumerable<Entity> entities, GameTime gameTime, SpriteBatch spriteBatch)
 		{
-			zoom += 0.001f;
-			rotation += 0.001f;
+			List<Entity> visibleEntities = GetVisibleEntites(entities);
 
-			GetVisableEntites(entities);
-
-			foreach (Entity entity in visableEntities)
+			foreach (Entity entity in visibleEntities)
 			{
 				entity.Draw(gameTime, spriteBatch, this);
-
-				// Also needs to pass the camera so it can get the transfom
-				//entity.Draw(gameTime, spriteBatch, this);
 			}
 		}
+
+		#endregion
 	}
 }
