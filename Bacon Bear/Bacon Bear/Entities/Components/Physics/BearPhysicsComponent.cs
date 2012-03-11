8fcb80bbf8e2eab097f870031fb47e3c484690df
Rocky Wilkins <rockywilkins@users.noreply.github.com>
2012-03-11 14:28:45 +0000

    Change Bear physics shaoe to polygon and prevent rotation

--- Bacon Bear/Bacon Bear/Entities/Components/Physics/BearPhysicsComponent.cs
+++ Bacon Bear/Bacon Bear/Entities/Components/Physics/BearPhysicsComponent.cs
@@ -1,14 +1,16 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input.Touch;
+using FarseerPhysics.Common;
+using FarseerPhysics.Collision.Shapes;
 using FarseerPhysics.Dynamics;
 using FarseerPhysics.Factories;
-using FarseerPhysics.Collision.Shapes;
 using Engine.Entities;
 using Engine.Input;
 using Engine.Physics;
 using BaconBear.Entities;
 using BaconBear.Entities.Components;
+
 
 namespace BaconBear.Entities.Components.Physics
 {
@@ -24,8 +26,19 @@ namespace BaconBear.Entities.Components.Physics
 				world = value as World;
 
 				body = BodyFactory.CreateBody(world);
+				body.FixedRotation = true;
 
-				CircleShape shape = new CircleShape(ConvertUnits.ToSimUnits(50f), 5f);
+				float width = ConvertUnits.ToSimUnits(68);
+				float height = ConvertUnits.ToSimUnits(46);
+
+				Vertices bounds = new Vertices(4);
+				bounds.Add(new Vector2(0, 0));
+				bounds.Add(new Vector2(width, 0));
+				bounds.Add(new Vector2(width, height));
+				bounds.Add(new Vector2(0, height));
+
+				PolygonShape shape = new PolygonShape(bounds, 5f);
+
 				Fixture fixture = body.CreateFixture(shape);
 
 				body.BodyType = BodyType.Dynamic;
