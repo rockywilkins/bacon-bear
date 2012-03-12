67511f9663ac3c7fda11cd6eb9b32e0767644062
Rocky Wilkins <rocky.wilkins@internetware.co.uk>
2012-03-12 22:34:44 +0000

    Add simple jump controls to Bacon Bear

--- Bacon Bear/Bacon Bear/EntityComponents/BearGraphicComponent.cs
+++ Bacon Bear/Bacon Bear/EntityComponents/BearGraphicComponent.cs
@@ -9,15 +9,36 @@ namespace BaconBear.Entities.Components.Graphics
 {
 	public class BearGraphicComponent : EntityComponent
 	{
+		private bool flipped = false;
+
 		public override void Draw(GameTime gameTime, SpriteBatch spriteBatch, Camera camera)
 		{
 			spriteBatch.Begin(SpriteSortMode.BackToFront, null, null, null, null, null, camera.Matrix);
 
 			Texture2D texture = Engine.Engine.Content.Load<Texture2D>("Textures/Bear");
 
-			spriteBatch.Draw(texture, new Rectangle((int)Parent.Position.X, (int)Parent.Position.Y, 68, 46), Color.White);
+			SpriteEffects effect = flipped ? SpriteEffects.FlipHorizontally : SpriteEffects.None;
+			spriteBatch.Draw(texture, new Rectangle((int)Parent.Position.X, (int)Parent.Position.Y, 68, 46), null, Color.White, 0, Vector2.Zero, effect, 0);
 
 			spriteBatch.End();
+		}
+
+		public override void ReceiveMessage(string name, object value)
+		{
+			if (name == "direction")
+			{
+				string direction = value as string;
+				if (direction == "right")
+				{
+					flipped = false;
+				}
+				else
+				{
+					flipped = true;
+				}
+			}
+
+			base.ReceiveMessage(name, value);
 		}
 	}
 }
