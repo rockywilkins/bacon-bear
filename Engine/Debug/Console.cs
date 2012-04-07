fc96a550d1b1242a56fbdd93c5438955e7787466
Chris Miller <3028071+Rugazi@users.noreply.github.com>
2012-04-07 16:00:28 +0100

    Added/Updated? Crappy console that works like crap

--- Engine/Debug/Console.cs
+++ Engine/Debug/Console.cs
@@ -1,57 +1,102 @@
+using System;
 using System.Collections.Generic;
 using Microsoft.Xna.Framework;
-using Engine.UI;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace Engine.Debug
 {
 	public static class Console
 	{
-		private static Game game;
-		private static TextRendering textRendering;
-		private static List<TextBlock> listTextBlocks = new List<TextBlock>();
-		private static int lastUpdate = 0;
-		private static int lines = 3;
+		private static Engine engine;
+		private static SpriteFont font;
+		private static List<Text> messages = new List<Text>();
+		private static List<Text> staticMessages = new List<Text>();
 
-		public static void Start(Game game)
+		public static void Start(Engine engine)
 		{
-			Console.game = game;
-
-			textRendering = (TextRendering)game.Services.GetService(typeof(TextRendering)); 
+		   Console.engine = engine;
+		   font = Engine.Content.Load<SpriteFont>("Fonts/Segoe");
 		}
-		
+
 		public static void Write(string text)
 		{
-			if (listTextBlocks.Count > lines -1)
-			{
-				listTextBlocks[lastUpdate].Text = text;
-				if (lastUpdate < lines - 1)
-					lastUpdate++;
-				else
-					lastUpdate = 0;
-			}
-			else
-			{
-				TextBlock textBlock = new TextBlock(text, new Vector2(10, listTextBlocks.Count * 20), Color.White, 0);
+			messages.Add(new Text { Message = text});
 
-				listTextBlocks.Add(textBlock);
-			}
+			if (messages.Count >= 10)
+				messages.RemoveAt(0);
 
-			foreach (TextBlock tb in listTextBlocks)
-			{
-				//if (!textRendering.TextBlocks.Contains(tb))
-				//    textRendering.TextBlocks.Add(tb);
-			}
+			UpdateMessageLocation();
 		}
 
-		public static void Write(string text, int line)
+		public static void Write(string id, string text)
 		{
-			if (line > lines)
-				line = lines;
+			bool newmsg = false;
 
-			if (listTextBlocks.Count >= line)
-				listTextBlocks[line - 1].Text = text;
-			else
-				Write(text);
+			foreach (Text txt in staticMessages)
+			{
+				if (txt.ID == id)
+				{
+					txt.Message = text;
+					newmsg = true;
+					return;
+				}
+			}
+
+			if (newmsg == false)
+			{
+				staticMessages.Add(new Text { ID = id, Message = text });
+				UpdateMessageLocation();
+			}
+
+			UpdateStaticMessageLocation();
 		}
+
+
+		private static void UpdateMessageLocation()
+		{
+			int count = 0;
+
+			if (staticMessages.Count > 0)
+				count = staticMessages.Count + 1;
+
+			foreach (Text txt in messages)
+			{
+				txt.Location = new Vector2(5f, count * 18);
+				count++;
+			}
+		}
+
+		private static void UpdateStaticMessageLocation()
+		{
+			int count = 0;
+
+			foreach (Text txt in staticMessages)
+			{
+				if (txt.Location == Vector2.Zero)
+					txt.Location = new Vector2(5f, count * 18);
+
+				count++;
+			}
+		}
+
+		public static void Draw(GameTime gameTime)
+		{
+			engine.SpriteBatch.Begin();
+
+			foreach (Text txt in staticMessages)
+				engine.SpriteBatch.DrawString(font, txt.Message, txt.Location, Color.Red);
+
+			foreach (Text txt in messages)
+				engine.SpriteBatch.DrawString(font, txt.Message, txt.Location, Color.Red);
+
+			engine.SpriteBatch.End();
+		}
+	}
+
+	public class Text
+	{
+		public string ID;
+		public string Message;
+		public Vector2 Location;
 	}
 }
